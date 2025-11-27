2951cbe4e20e4b699f47872d167f909d84fcb61e
payrollpro-backend <contracting@goodfellasholdings.com>
2025-11-26 21:08:08 -0500

    Modify PaystubSchema to relax field requirements
    Removed 'required' validation from payDate and fileName fields in PaystubSchema.

--- src/models/Paystub.js
+++ src/models/Paystub.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
-const PaystubSchema = new mongoose.Schema(
-  {
-    employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
-    payrollRun: { type: mongoose.Schema.Types.ObjectId, ref: 'PayrollRun', required: true },
-    payDate: { type: Date, required: true },
-    fileName: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const PaystubSchema = new mongoose.Schema({
+  employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+  payrollRun: { type: mongoose.Schema.Types.ObjectId, ref: 'PayrollRun', required: true },
+  payDate: Date,
+  fileName: String,
+}, { timestamps: true });
 
 module.exports = mongoose.model('Paystub', PaystubSchema);
